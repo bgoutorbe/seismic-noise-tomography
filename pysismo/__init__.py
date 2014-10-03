52e04506bfbbf8517cc27939af3867b422a43cbe
bgoutorbe <goutorbe@hotmail.com>
2014-10-03 16:50:46 -0300

    minor corrections

--- pysismo/__init__.py
+++ pysismo/__init__.py
@@ -1,1 +1,16 @@
-import pscrosscorr, pserrors, psspectrum, psstation, psutils, pstomo
+import psconfig
+
+# Not importing other modules here so that changing parameters in
+# psconfig will take effect when importing other modules after.
+# In other words, if for some reason you want to change some
+# default parameters without touching the configuration file,
+# you can do something like:
+#
+# >>> from pysismo import psconfig
+# >>> psconfig.PERIOD_BANDS = [[5, 20], [10, 50]]
+# >>> from pysismo import pscrosscorr
+#
+# In this example pscrosscorr will be initialized with the
+# modified value of the parameter *PERIOD_BANDS*.
+
+# import pscrosscorr, pserrors, psspectrum, psstation, psutils, pstomo
